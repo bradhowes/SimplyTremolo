470ca00f12970c3714ee89bfb03c967bd0581205
Brad Howes <bradhowes@mac.com>
2023-12-17 20:04:20 +0100

    Use AUv3Support v9.0.0

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "Packages",
-  platforms: [.iOS(.v13), .macOS(.v10_15)],
+  platforms: [.iOS(.v13), .macOS(.v11)],
   products: [
     .library(name: "KernelBridge", targets: ["KernelBridge"]),
     .library(name: "Kernel", targets: ["Kernel"]),
@@ -13,7 +13,7 @@ let package = Package(
     .library(name: "Theme", targets: ["Theme"])
   ],
   dependencies: [
-    .package(name: "AUv3SupportPackage", url: "https://github.com/bradhowes/AUv3Support", branch: "main"),
+    .package(name: "AUv3SupportPackage", url: "https://github.com/bradhowes/AUv3Support", from: "9.0.0"),
   ],
   targets: [
     .target(
@@ -68,5 +68,5 @@ let package = Package(
       dependencies: ["Parameters"]
     ),
   ],
-  cxxLanguageStandard: .cxx17
+  cxxLanguageStandard: .cxx20
 )
